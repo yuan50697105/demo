03f210e0bcf760e9caa83debe1668c8dc62b07e3
yuanenzhi <710575900@qq.com>
2022-09-23 09:10:24 +0800

    .

--- src/main/java/com/example/demo/entity/SysUser.java
+++ src/main/java/com/example/demo/entity/SysUser.java
@@ -2,15 +2,26 @@ package com.example.demo.entity;
 
 import cn.org.atool.fluent.mybatis.annotation.FluentMybatis;
 import cn.org.atool.fluent.mybatis.base.RichEntity;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.fhs.core.trans.vo.TransPojo;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import javax.persistence.Id;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @FluentMybatis
 public class SysUser extends RichEntity implements TransPojo {
+    @Id
+    @TableId(type = IdType.INPUT)
+    @cn.org.atool.fluent.mybatis.annotation.TableId
     private String id;
+    private String username;
+    private String name;
+    private String password;
+    private Integer state;
 
     public SysUser(String id) {
         this.id = id;
